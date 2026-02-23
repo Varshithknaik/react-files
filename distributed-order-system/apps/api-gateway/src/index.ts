1c4d129118ebce041097b6e6ef85de1d61e3f869
Varshith K <varshithknaik@gmail.com>
2026-02-23 21:56:15 +0530

    feat: update Docker configurations, enhance API gateway, and add new scripts for local development

--- distributed-order-system/apps/api-gateway/src/index.ts
+++ distributed-order-system/apps/api-gateway/src/index.ts
@@ -3,35 +3,37 @@ import cors from 'cors'
 import { createProxyMiddleware } from 'http-proxy-middleware'
 import { logger } from '@core/logger'
 import dotenv from 'dotenv'
+import path from 'path'
 
 dotenv.config({ quiet: true })
+
+const PORT = process.env.PORT || 4000
 
 const app = express()
 app.use(cors())
 
+const fePath = path.join(__dirname, '../../web-client/dist')
+app.use(express.static(fePath))
+
+app.use((_, res) => {
+  res.sendFile(path.join(fePath, 'index.html'))
+})
+
 app.use(
   '/commands',
   createProxyMiddleware({
-    target: 'http://order-service:3001',
-    pathRewrite: {
-      '^/commands': '',
-    },
+    target: 'http://localhost:3001',
+    pathRewrite: { '^/commands': '' },
   })
 )
 
 app.use(
   '/queries',
   createProxyMiddleware({
-    target: 'http://read-service:3004',
-    pathRewrite: {
-      '^/queries': '',
-    },
+    target: 'http://localhost:3004',
+    pathRewrite: { '^/queries': '' },
   })
 )
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.get('/health', (req, res) => {
   res.send({
@@ -41,6 +43,6 @@ app.get('/health', (req, res) => {
   })
 })
 
-app.listen(4000, () => {
-  logger.info('API Gateway listening on port 4000')
+app.listen(PORT, () => {
+  logger.info(`API Gateway listening on port ${PORT}`)
 })
