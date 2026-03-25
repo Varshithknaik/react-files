0ea94cae13ea5abedc0a72875d81ec3a9a535449
Varshith K <varshithknaik@gmail.com>
2026-03-25 20:40:20 +0530

    prisms migration for order-service

--- distributed-order-system/apps/order-service/src/index.ts
+++ distributed-order-system/apps/order-service/src/index.ts
@@ -1,28 +1,16 @@
 import express from 'express'
-import pkg from 'pg'
 import dotenv from 'dotenv'
 import { KafkaClient } from '@core/kafka'
-import { v4 as uuid } from 'uuid'
-import { OrderTopics } from '@core/events'
 import { logger } from '@core/logger'
+import grpc from "@grpc/grpc-js"
 
 dotenv.config({ quiet: true })
 
 const app = express()
 app.use(express.json())
 
-export async function initDB() {
-  await pool.query(`
-    CREATE SCHEMA IF NOT EXISTS order_service;
-  `)
-}
+const server = new grpc.Server()
 
-const pool = new pkg.Pool({
-  connectionString: process.env.ORDER_DB_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-})
 
 const kafka = new KafkaClient('order-service', [process.env.KAFKA_BROKERS!])
 const producer = kafka.createProducer()
@@ -35,46 +23,7 @@ app.get('/health', (req, res) => {
   })
 })
 
-app.post('/orders', async (req, res) => {
-  logger.info('Creating order')
-  const { userId, total } = req.body
-  const orderId = uuid()
-  const client = await pool.connect()
-  logger.info('Creating order')
-  try {
-    await client.query('BEGIN')
-
-    await client.query(
-      'INSERT INTO orders (id, user_id, total , status) VALUES ($1, $2, $3 , $4)',
-      [orderId, userId, total, 'PENDING']
-    )
-
-    await producer.connect()
-    await producer.send({
-      topic: OrderTopics.ORDER_LIFECYCLE,
-      messages: [
-        {
-          key: orderId,
-          value: JSON.stringify({
-            orderId,
-            userId,
-            total,
-            status: 'PENDING',
-          }),
-        },
-      ],
-    })
-    logger.info('Order created', orderId)
-    res.status(201).send({ orderId })
-  } catch (e) {
-    await client.query('ROLLBACK')
-    res.status(500).send(e)
-  } finally {
-    client.release()
-  }
-})
 
 app.listen(3001, async () => {
-  await initDB()
   logger.info(`API Gateway listening on port 3001`)
 })
