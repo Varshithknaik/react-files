392aacfc7448d1c546b4a134c070fd95ce94e6bf
Varshith K <varshithknaik@gmail.com>
2026-05-30 14:09:19 +0530

    feat: implement header and navigation structure for meals page

--- nextjs-learn/05-onwards-foodies-starting-project/app/meals/page.js
+++ nextjs-learn/05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,3 +1,19 @@
-export default function MealsPage(){
-    return <h1>Meals page</h1>;
+import classes from './page.module.css'
+
+export default function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. Easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  )
 }
