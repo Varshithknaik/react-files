a3f75f1b08c2c6cd6b960dd6cf53f518e0ead8ae
Varshith K <varshithknaik@gmail.com>
2026-06-09 22:20:43 +0530

    feat: implement form validation and submission state handling for meal sharing

--- nextjs-learn/05-onwards-foodies-starting-project/app/meals/share/page.js
+++ nextjs-learn/05-onwards-foodies-starting-project/app/meals/share/page.js
@@ -3,8 +3,11 @@
 import classes from './page.module.css'
 import ImagePicker from '@/components/meals/image-picker'
 import { shareMeal } from '@/lib/action'
+import MealsFormSubmit from '@/components/meals/meals-form-submit'
+import { useFormState } from 'react-dom'
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null })
   return (
     <>
       <header className={classes.header}>
@@ -14,15 +17,15 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
+              <input type="text" id="name" name="name" />
             </p>
             <p>
               <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
+              <input type="email" id="email" name="email" />
             </p>
           </div>
           <p>
@@ -43,8 +46,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Pick an Image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
---
+++ nextjs-learn/05-onwards-foodies-starting-project/components/meals/meals-form-submit.js
@@ -0,0 +1,12 @@
+'use client'
+
+import { useFormStatus } from 'react-dom'
+
+export default function MealsFormSubmit() {
+  const status = useFormStatus()
+  return (
+    <button type="submit" disabled={status.pending}>
+      {status.pending ? 'Submitting...' : 'Share Meal'}
+    </button>
+  )
+}
