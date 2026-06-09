0dd2480a3f02f68013be44f58ef89dd0c77921fe
Varshith K <varshithknaik@gmail.com>
2026-06-09 21:15:51 +0530

    feat: implement meal submission logic with image storage, input sanitization, and database integration

--- nextjs-learn/05-onwards-foodies-starting-project/lib/action.js
+++ nextjs-learn/05-onwards-foodies-starting-project/lib/action.js
@@ -1,5 +1,8 @@
 'use server'
 
+import { redirect } from 'next/navigation'
+import { saveMeal } from './meal'
+
 export async function shareMeal(formData) {
   const meal = {
     title: formData.get('title'),
@@ -9,5 +12,6 @@ export async function shareMeal(formData) {
     creator_email: formData.get('email'),
     image: formData.get('image'),
   }
-  console.log(meal)
+  await saveMeal(meal)
+  redirect('/meals')
 }
