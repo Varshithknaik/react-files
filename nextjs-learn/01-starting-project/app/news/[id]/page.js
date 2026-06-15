17b3cc043d725020400d682034cb21cc17b0be1a
Varshith K <varshithknaik@gmail.com>
2026-06-15 21:58:57 +0530

    feat: add news listing page and dynamic news detail route

---
+++ nextjs-learn/01-starting-project/app/news/[id]/page.js
@@ -0,0 +1,3 @@
+export default function NewsDetails({ params }) {
+  return <h1>News Page {params.id}</h1>
+}
---
+++ nextjs-learn/01-starting-project/app/news/page.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+export default function News() {
+  return (
+    <>
+      <h1> News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/1">News 1</Link>
+        </li>
+        <li>
+          <Link href="/news/2">News 2</Link>
+        </li>
+        <li>
+          <Link href="/news/3">News 3</Link>
+        </li>
+      </ul>
+    </>
+  )
+}
