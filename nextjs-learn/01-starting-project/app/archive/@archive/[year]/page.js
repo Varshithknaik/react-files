6d0f29d352e0ed02d2903c49b03e593d77a22581
Varshith K <varshithknaik@gmail.com>
2026-06-16 20:24:20 +0530

    refactor: consolidate archive routing into a catch-all route and clean up unused console logs in news-list

--- nextjs-learn/01-starting-project/app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import { getNewsForYear } from '@/lib/news'
-import NewsList from '@/components/news-list'
-
-export default function FilteredNewsPage({ params }) {
-  const year = params.year
-  const news = getNewsForYear(year)
-  return <NewsList news={news} />
-}
--- nextjs-learn/01-starting-project/components/news-list.js
+++ nextjs-learn/01-starting-project/components/news-list.js
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function NewsList({ news }) {
-  console.log(news)
   return (
     <ul className="news-list">
       {news.map((singleNewsItem) => (
