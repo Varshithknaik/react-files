785e5b86357ca9c400a77b3b511a1363cbc158d2
Varshith K <varshithknaik@gmail.com>
2026-07-07 20:29:53 +0530

    feat: implement Cloudinary image upload in server actions and refactor form handling logic

---
+++ nextjs-learn/06-data-mutation-starting-project/actions/posts.js
@@ -0,0 +1,47 @@
+'use server'
+
+import { uploadImage } from '@/lib/cloudinary'
+import { storePost } from '@/lib/posts'
+import { redirect } from 'next/navigation'
+
+export async function createPost(prevState, formData) {
+  const title = formData.get('title')
+  const image = formData.get('image')
+  const content = formData.get('content')
+
+  let errors = []
+
+  if (!title || title.trim().length === 0) {
+    errors.push('Title is required.')
+  }
+
+  if (!content || content.trim().length === 0) {
+    errors.push('Content is required.')
+  }
+
+  if (!image || image.size === 0) {
+    errors.push('Image is required.')
+  }
+
+  if (errors.length > 0) {
+    return { errors }
+  }
+
+  let uploadedImage
+
+  try {
+    uploadedImage = await uploadImage(image)
+    console.log(uploadedImage)
+  } catch (error) {
+    throw new Error('Error uploading image.')
+  }
+
+  await storePost({
+    title,
+    imageUrl: uploadedImage,
+    content,
+    userId: 1,
+  })
+
+  redirect('/feed')
+}
