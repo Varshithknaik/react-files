8271657f257c96dacaa94cf292eeb2c0af81fbe0
Varshith K <varshithknaik@gmail.com>
2026-07-23 21:28:33 +0530

    feat: initialize authentication app and implement dynamic SEO metadata for feed page

--- nextjs-learn/08-SEO-optimization/app/feed/page.js
+++ nextjs-learn/08-SEO-optimization/app/feed/page.js
@@ -1,12 +1,26 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from '@/components/posts'
+import { getPosts } from '@/lib/posts'
+
+// export const metadata = {
+//   title: 'All posts',
+//   description: 'Browse all posts by all users',
+// }
+
+export async function generateMetadata() {
+  const posts = await getPosts()
+  const numberOfPosts = posts.length
+  return {
+    title: `Browse all our ${numberOfPosts} posts`,
+    description: 'Browse all posts by all users',
+  }
+}
 
 export default async function FeedPage() {
-  const posts = await getPosts();
+  const posts = await getPosts()
   return (
     <>
       <h1>All posts by all users</h1>
       <Posts posts={posts} />
     </>
-  );
+  )
 }
